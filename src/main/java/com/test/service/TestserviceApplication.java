4199adba90b617dbcbc2196eaae748a4841bf45e
shafeeq.bari <shafeeq.bari@dish.com>
2022-01-10 08:53:13 -0700

    Initial

---
+++ src/main/java/com/test/service/TestserviceApplication.java
@@ -0,0 +1,14 @@
+package com.test.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestserviceApplication {
+
+    public static void main(String[] args) {
+        
+        SpringApplication.run(TestserviceApplication.class, args);
+    }
+
+}
